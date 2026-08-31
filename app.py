1d5471a97f2da9f1bdcd7b5964c713fad4e2c11d
sufyaan-nvs <sufyaan.talish_ext@novartis.com>
2026-08-31 16:28:21 +0530

    Fix iframe scrolling parameter in app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(layout="wide")
-
 components.iframe(
     "https://copilotstudio.microsoft.com/environments/a32fc343-9406-e5c7-8c34-8f2bf7608657/bots/cr075_untitledagent_fRHMcV/webchat?__version__=2&enableFileAttachment=false&cliAgent=true",
     height=900,
-    scrolling=True,
+    scrolling=True
 )
